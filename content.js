eb486dcd8878190a93ff52b70d088b6e556cb017
esp <swetha.priya.eddula@sap.com>
2018-11-15 17:42:46 +0530

    Removing commented code in v1

--- content.js
+++ content.js
@@ -1,4 +1,3 @@
 document.getElementById("app").addEventListener("click", function(){
     document.getElementsByClassName("_39LWd")[0].innerHTML = "esp text suggest";
 });
-// console.log(document.getElementsByTagName( "div" ).length);.getElementsByTagName("div")[0].getElementsByClassName("_39LWd")[0].innerHTML
