1d7feb042ebd046801da5f77ad0710474c7847ed
esp <swetha.priya.eddula@sap.com>
2018-11-15 17:40:31 +0530

    esp text suggest v1. Alters "Type message here" to "esp text suggest.

---
+++ content.js
@@ -0,0 +1,4 @@
+document.getElementById("app").addEventListener("click", function(){
+    document.getElementsByClassName("_39LWd")[0].innerHTML = "esp text suggest";
+});
+// console.log(document.getElementsByTagName( "div" ).length);.getElementsByTagName("div")[0].getElementsByClassName("_39LWd")[0].innerHTML
